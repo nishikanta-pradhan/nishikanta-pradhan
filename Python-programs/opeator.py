ffa09803364d8f78a78db748031c10f7af48a9b9
Nishikanta Pradhan <nishimuna@gmail.com>
2026-04-20 22:14:12 +0530

    python programs

---
+++ Python-programs/opeator.py
@@ -0,0 +1,13 @@
+# Arithmetic operation
+a = int(input("Enter number a : "))
+b = int(input("Enter number b : "))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("Arithmetic Opeations")
+print("-------------------------")
+print("Addition of",a ,"&", b," = ",add)
+print("Substraction of",a ,"&", b," = ",sub)
+print("Multiplication of",a ,"&", b," = ",mul)
+print("Division of",a ,"&", b," = ",div)
